122fc69f2d854e105b697cfc70d6bf297252255d
Luisa Andrielly <luisa.2021315963@aluno.iffar.edu.br>
2023-11-10 09:29:00 -0300

    Create listarItens.php

---
+++ listarItens.php
@@ -0,0 +1,20 @@
+<?php
+include_once "conectar.php";
+$conexao = conectar();
+
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM itens WHERE id = $id";
+} else {
+    $sql = "SELECT * FROM itens";
+}
+
+$resultado = mysqli_query($conexao, $sql);
+
+if ($resultado == TRUE) {
+    $itens = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+    echo json_encode($itens);
+} else {
+    die("Erro ao buscar os dados dos itens." . mysqli_errno($conexao) . ":" . mysqli_error($conexao));
+}
+?>
