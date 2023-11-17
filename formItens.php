07f24e7c787b459a62c2766d1e875e9889d758e6
Luisa Andrielly <luisa.2021315963@aluno.iffar.edu.br>
2023-11-17 09:30:40 -0300

    Update formItens.php

--- formItens.php
+++ formItens.php
@@ -1,181 +1,199 @@
-<!-- Parte do formulário para cadastrar/editar itens -->
-<div class="container col s6 offset-s3 borda orange lighten-5">
-    <h3 class="center-align titulo">Cadastrar/Editar Item</h3>
-    <form id="form" onsubmit="return salvarItem(event);">
-        <div class="input-field prefix-active">
-            <label class="active" for="id">ID</label>
-            <input type="text" name="id" required readonly>
-        </div>
+<!DOCTYPE html>
+<html lang="pt-BR">
 
-        <div class="input-field prefix-active">
-            <label for="nome">Nome</label>
-            <input type="text" name="nome" required>
-        </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD de Itens</title>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+      <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
+    <link rel="stylesheet" href="css/estilo.css">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <div class="input-field prefix.active">
-            <label for="descricao">Descrição</label>
-            <input type="text" name="descricao" required>
-        </div>
+    <!-- Baixa e usa CSS na CDN-->
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">-->
 
-        <div class="input-field prefix.active">
-            <label for="preco">Preço</label>
-            <input type="text" name="preco" required>
-        </div>
+    <!-- Compiled and minified JavaScript -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>-->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+ </head> 
 
-        <p class="center-align">
-            <button class="waves-effect waves-light btn #00695c teal darken-3 white-text brown lighten-1" type="submit">
-                <i class="material-icons left"></i>Cadastrar
-            </button>
-        </p>
+<body>
+    <h2>CRUD de Itens</h2> 
+
+    Formulário de Inserção
+    <form id="formInserir">
+        <label for="nome">Nome do Item:</label>
+        <input type="text" id="nome" name="nome" required><br>
+
+        <label for="descricao">Descrição:</label>
+        <input type="text" id="descricao" name="descricao" required><br>
+
+        <label for="preco">Preço:</label>
+        <input type="text" id="preco" name="preco" required><br>
+
+        <button type="submit">Inserir Item</button>
     </form>
-</div>
 
-<!-- Tabela para listar itens -->
-<div class="row">
-    <article class="col s10 offset-s1">
-        <table class="responsive-table striped ftabela">
-            <thead class="titulo">
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Descrição</th>
-                    <th>Preço</th>
-                    <th colspan="2">Opções</th>
-                </tr>
-            </thead>
-            <tbody id="itens">
-                <!-- Os itens serão listados aqui dinamicamente -->
-            </tbody>
-        </table>
-    </article>
-</div>
+    <hr>
 
-<!-- Modais -->
-<div id="modalEditar" class="modal">
-    <!-- Conteúdo do modal de edição -->
-</div>
+    <!-- Tabela de Celulares -->
+    <table border="1" id="tabelaItens">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody id="listaItens">
+            <!-- A lista de celulares será carregada aqui usando Ajax -->
+        </tbody>
+    </table>
 
-<div id="modalExcluir" class="modal">
-    <div class="modal-content">
-        <h4>Excluir Item</h4>
-        <p>Tem certeza que deseja excluir o item?</p>
+    <!-- Modal de Edição -->
+    <div id="modalEditar" class="modal">
+        <div class="modal-content">
+            <h4>Editar Item</h4>
+            <form id="formEditar">
+                <input type="hidden" id="editId" name="id">
+                <label for="editNome">Marca:</label>
+                <input type="text" id="editNome" name="nome" required><br>
+
+                <label for="editDescricao">Descrição:</label>
+                <input type="text" id="editDescricao" name="descricao" required><br>
+
+                <label for="editPreco">Preço:</label>
+                <input type="text" id="editPreco" name="preco" required><br>
+
+                <button type="submit">Salvar</button>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
+        </div>
     </div>
-    <div class="modal-footer center">
-        <button id="btn-sim" class="modal-close waves-effect waves-green btn-flat" onclick="excluir();">Sim</button>
-        <a href="#!" class="modal-close waves-effect waves-red btn-flat">Não</a>
-    </div>
-</div>
 
     <script>
-        $(document).ready(function() {
-            listarItens();
-        });
+        $(document).ready(function () {
+    function carregarCelulares() {
+        $.ajax({
+            url: 'listar_itens.php',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                var listaCelulares = $('#listaItens');
+                listaCelulares.empty();
 
-        function listarItens() {
-            $.ajax({
-                url: 'listarItens.php',
-                type: 'GET',
-                dataType: 'json',
-                success: function(data) {
-                    exibirItens(data);
-                },
-                error: function(error) {
-                    console.log(error);
+                for (var i = 0; i < data.length; i++) {
+                    var botaoEditar = '<button class="btn-editar" data-id="' + data[i].id + '" data-marca="' + data[i].marca +
+                        '" data-modelo="' + data[i].modelo + '" data-preco="' + data[i].preco + '">Editar</button>';
+
+                    var botaoExcluir = '<button class="btn-excluir" data-id="' + data[i].id + '">Excluir</button>';
+
+                    listaCelulares.append('<tr><td>' + data[i].id + '</td><td>' + data[i].marca + '</td><td>' + data[i].modelo +
+                        '</td><td>' + data[i].preco +
+                        '</td><td>' + botaoEditar + ' ' + botaoExcluir + '</td></tr>');
                 }
-            });
-        }
 
-        function exibirItens(itens) {
-            var tabela = $('#tabelaItens');
-            tabela.empty();
+                // Adiciona o evento de click para o botão excluir
+                $('.btn-excluir').click(function () {
+                    var id = $(this).data('id');
+                    if (confirm("Tem certeza que deseja excluir este celular?")) {
+                        excluirCelular(id);
+                    }
+                });
 
-            for (var i = 0; i < itens.length; i++) {
-                var item = itens[i];
-                var linha = $('<tr>');
-                linha.append('<td>' + item.id + '</td>');
-                linha.append('<td>' + item.nome + '</td>');
-                linha.append('<td>' + item.descricao + '</td>');
-                linha.append('<td>' + item.preco + '</td>');
-                linha.append('<td><button onclick="editarItens(' + item.id + ')">Editar</button> | <button onclick="excluirItens(' + item.id + ')">Excluir</button></td>');
-
-                tabela.append(linha);
+                // Inicializa o modal
+                var modals = document.querySelectorAll('.modal');
+                M.Modal.init(modals);
+            },
+            error: function (xhr, status, error) {
+                console.error('Erro ao carregar a lista de celulares: ' + error);
             }
+        });
+    }
+
+    $('#formInserir').submit(function (e) {
+        e.preventDefault();
+        var formData = $(this).serialize();
+
+        $.ajax({
+            url: 'inserir_celular.php',
+            type: 'POST',
+            data: formData,
+            success: function () {
+                carregarCelulares();
+                $('#formInserir')[0].reset();
+            },
+            error: function (xhr, status, error) {
+                console.error('Erro ao inserir o celular: ' + error);
+            }
+        });
+    });
+
+    $('#formEditar').submit(function (e) {
+        e.preventDefault();
+        var formData = $(this).serialize();
+
+        $.ajax({
+            url: 'editar_celular.php',
+            type: 'POST',
+            data: formData,
+            success: function () {
+                carregarCelulares();
+                M.Modal.getInstance($('#modalEditar')).close(); // Feche o modal após a edição
+            },
+            error: function (xhr, status, error) {
+                console.error('Erro ao editar o celular: ' + error);
+            }
+        });
+    });
+
+    $(document).on('click', '.btn-editar', function () {
+        var id = $(this).data('id');
+        var marca = $(this).data('marca');
+        var modelo = $(this).data('modelo');
+        var preco = $(this).data('preco');
+        preencherFormEditar(id, marca, modelo, preco);
+    });
+
+    carregarCelulares();
+});
+
+function excluirCelular(id) {
+    $.ajax({
+        url: 'excluir_celular.php',
+        type: 'POST',
+        data: { id: id },
+        success: function () {
+            carregarCelulares();
+        },
+        error: function (xhr, status, error) {
+            console.error('Erro ao excluir o celular: ' + error);
         }
+    });
+}
 
-        function salvarItem(event) {
-            event.preventDefault();
-            
-            var formData = $('#formItens').serializeArray();
-            var item = {};
-            
-            $.each(formData, function(i, field) {
-                item[field.name] = field.value;
-            });
+function preencherFormEditar(id, marca, modelo, preco) {
+    $('#editId').val(id);
+    $('#editMarca').val(marca);
+    $('#editModelo').val(modelo);
+    $('#editPreco').val(preco);
+    M.Modal.getInstance($('#modalEditar')).open(); // Abra o modal usando getInstance
+}
 
-            var url = item.id ? 'salvarItens.php' : 'salvarItens.php';
 
-            $.ajax({
-                url: url,
-                type: item.id ? 'PUT' : 'POST',
-                dataType: 'json',
-                data: JSON.stringify(item),
-                contentType: 'application/json',
-                success: function(data) {
-                    listarItens();
-                    $('#formItens')[0].reset();
-                },
-                error: function(error) {
-                    console.log(error);
-                }
-            });
-
-            return false;
-        }
-
-        function editarItem(id) {
-            $.ajax({
-                url: 'listarItens.php?id=' + id,
-                type: 'GET',
-                dataType: 'json',
-                success: function(data) {
-                    preencherFormulario(data);
-                },
-                error: function(error) {
-                    console.log(error);
-                }
-            });
-        }
-
-        function excluirItem(id) {
-            $('#modal-confirmacao').modal('open');
-            idExclusao = id;
-            $.ajax({
-                url: 'excluirItens.php?id=' + id,
-                type: 'DELETE',
-                dataType: 'json',
-                success: function(data) {
-                    listarItens();
-                },
-                error: function(error) {
-                    console.log(error);
-                }
-            });
-        }
-
-        function excluir() {
-            // Fecha o modal de confirmação
-            $('#modal-confirmacao').modal('close');
-
-            // Chama a função para excluir o item
-            excluirItem(idExclusao);
-        }
-
-        function preencherFormulario(item) {
-            $('input[name="id"]').val(item.id);
-            $('input[name="nome"]').val(item.nome);
-            $('input[name="descricao"]').val(item.descricao);
-            $('input[name="preco"]').val(item.preco);
-        }
+            carregarCelulares();
+        // });
     </script>
+     <script type="text/javascript" src="js/materialize.js"></script>
 </body>
+
 </html>
