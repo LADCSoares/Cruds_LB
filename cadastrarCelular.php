d39f9db51d5712653434b9c4398642c9aac6869c
befloresfarias <bernardo.2021319283@aluno.iffar.edu.br>
2023-11-17 09:24:12 -0300

    Update cadastrarCelular.php

--- cadastrarCelular.php
+++ cadastrarCelular.php
@@ -6,13 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD de Celulares</title>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    
-</head>
+      <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
+    <link rel="stylesheet" href="css/estilo.css">
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Baixa e usa CSS na CDN-->
+    <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">-->
+
+    <!-- Compiled and minified JavaScript -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>-->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+ </head> 
 
 <body>
-    <h2>CRUD de Celulares</h2>
+    <h2>CRUD de Celulares</h2> 
 
-    <!-- Formulário de Inserção -->
+    Formulário de Inserção
     <form id="formInserir">
         <label for="marca">Marca:</label>
         <input type="text" id="marca" name="marca" required><br>
@@ -69,108 +82,118 @@
 
     <script>
         $(document).ready(function () {
-            function carregarCelulares() {
-                $.ajax({
-                    url: 'listar_celulares.php',
-                    type: 'GET',
-                    dataType: 'json',
-                    success: function (data) {
-                        var listaCelulares = $('#listaCelulares');
-                        listaCelulares.empty();
+    function carregarCelulares() {
+        $.ajax({
+            url: 'listar_celulares.php',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                var listaCelulares = $('#listaCelulares');
+                listaCelulares.empty();
 
-                        for (var i = 0; i < data.length; i++) {
-                            var botaoEditar = '<button class="btn-editar" data-id="' + data[i].id + '" data-marca="' + data[i].marca +
-                                '" data-modelo="' + data[i].modelo + '" data-preco="' + data[i].preco + '">Editar</button>';
+                for (var i = 0; i < data.length; i++) {
+                    var botaoEditar = '<button class="btn-editar" data-id="' + data[i].id + '" data-marca="' + data[i].marca +
+                        '" data-modelo="' + data[i].modelo + '" data-preco="' + data[i].preco + '">Editar</button>';
 
-                            var botaoExcluir = '<button class="btn-excluir" data-id="' + data[i].id + '">Excluir</button>';
+                    var botaoExcluir = '<button class="btn-excluir" data-id="' + data[i].id + '">Excluir</button>';
 
-                            listaCelulares.append('<tr><td>' + data[i].id + '</td><td>' + data[i].marca + '</td><td>' + data[i].modelo +
-                                '</td><td>' + data[i].preco +
-                                '</td><td>' + botaoEditar + ' ' + botaoExcluir + '</td></tr>');
-                        }
-                    },
-                    error: function (xhr, status, error) {
-                        console.error('Erro ao carregar a lista de celulares: ' + error);
-                    }
-                });
-            }
-
-            $('#formInserir').submit(function (e) {
-                e.preventDefault();
-                var formData = $(this).serialize();
-
-                $.ajax({
-                    url: 'inserir_celular.php',
-                    type: 'POST',
-                    data: formData,
-                    success: function () {
-                        carregarCelulares();
-                        $('#formInserir')[0].reset();
-                    },
-                    error: function (xhr, status, error) {
-                        console.error('Erro ao inserir o celular: ' + error);
-                    }
-                });
-            });
-
-            $('#formEditar').submit(function (e) {
-                e.preventDefault();
-                var formData = $(this).serialize();
-
-                $.ajax({
-                    url: 'editar_celular.php',
-                    type: 'POST',
-                    data: formData,
-                    success: function () {
-                        carregarCelulares();
-                        $('#modalEditar').modal('close');
-                    },
-                    error: function (xhr, status, error) {
-                        console.error('Erro ao editar o celular: ' + error);
-                    }
-                });
-            });
-
-            $(document).on('click', '.btn-editar', function () {
-                var id = $(this).data('id');
-                var marca = $(this).data('marca');
-                var modelo = $(this).data('modelo');
-                var preco = $(this).data('preco');
-                preencherFormEditar(id, marca, modelo, preco);
-            });
-
-            $(document).on('click', '.btn-excluir', function () {
-                var id = $(this).data('id');
-                if (confirm("Tem certeza que deseja excluir este celular?")) {
-                    excluirCelular(id);
+                    listaCelulares.append('<tr><td>' + data[i].id + '</td><td>' + data[i].marca + '</td><td>' + data[i].modelo +
+                        '</td><td>' + data[i].preco +
+                        '</td><td>' + botaoEditar + ' ' + botaoExcluir + '</td></tr>');
                 }
-            });
 
-            function excluirCelular(id) {
-                $.ajax({
-                    url: 'excluir_celular.php',
-                    type: 'POST',
-                    data: { id: id },
-                    success: function () {
-                        carregarCelulares();
-                    },
-                    error: function (xhr, status, error) {
-                        console.error('Erro ao excluir o celular: ' + error);
+                // Adiciona o evento de click para o botão excluir
+                $('.btn-excluir').click(function () {
+                    var id = $(this).data('id');
+                    if (confirm("Tem certeza que deseja excluir este celular?")) {
+                        excluirCelular(id);
                     }
                 });
-            }
 
-            function preencherFormEditar(id, marca, modelo, preco) {
-                $('#editId').val(id);
-                $('#editMarca').val(marca);
-                $('#editModelo').val(modelo);
-                $('#editPreco').val(preco);
-                $('#modalEditar').modal('open');
+                // Inicializa o modal
+                var modals = document.querySelectorAll('.modal');
+                M.Modal.init(modals);
+            },
+            error: function (xhr, status, error) {
+                console.error('Erro ao carregar a lista de celulares: ' + error);
             }
+        });
+    }
+
+    $('#formInserir').submit(function (e) {
+        e.preventDefault();
+        var formData = $(this).serialize();
+
+        $.ajax({
+            url: 'inserir_celular.php',
+            type: 'POST',
+            data: formData,
+            success: function () {
+                carregarCelulares();
+                $('#formInserir')[0].reset();
+            },
+            error: function (xhr, status, error) {
+                console.error('Erro ao inserir o celular: ' + error);
+            }
+        });
+    });
+
+    $('#formEditar').submit(function (e) {
+        e.preventDefault();
+        var formData = $(this).serialize();
+
+        $.ajax({
+            url: 'editar_celular.php',
+            type: 'POST',
+            data: formData,
+            success: function () {
+                carregarCelulares();
+                M.Modal.getInstance($('#modalEditar')).close(); // Feche o modal após a edição
+            },
+            error: function (xhr, status, error) {
+                console.error('Erro ao editar o celular: ' + error);
+            }
+        });
+    });
+
+    $(document).on('click', '.btn-editar', function () {
+        var id = $(this).data('id');
+        var marca = $(this).data('marca');
+        var modelo = $(this).data('modelo');
+        var preco = $(this).data('preco');
+        preencherFormEditar(id, marca, modelo, preco);
+    });
+
+    carregarCelulares();
+});
+
+function excluirCelular(id) {
+    $.ajax({
+        url: 'excluir_celular.php',
+        type: 'POST',
+        data: { id: id },
+        success: function () {
+            carregarCelulares();
+        },
+        error: function (xhr, status, error) {
+            console.error('Erro ao excluir o celular: ' + error);
+        }
+    });
+}
+
+function preencherFormEditar(id, marca, modelo, preco) {
+    $('#editId').val(id);
+    $('#editMarca').val(marca);
+    $('#editModelo').val(modelo);
+    $('#editPreco').val(preco);
+    M.Modal.getInstance($('#modalEditar')).open(); // Abra o modal usando getInstance
+}
+
 
             carregarCelulares();
-        });
+        // });
     </script>
+     <script type="text/javascript" src="js/materialize.js"></script>
 </body>
 
 </html>
