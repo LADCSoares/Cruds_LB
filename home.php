1cab2991d6d38a11505f0951a31292eaa7c83523
Luisa Andrielly <luisa.2021315963@aluno.iffar.edu.br>
2023-12-09 01:13:16 -0300

    Update home.php

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD de Usuários</title>
+    <title>Crud Usuário</title>
     <meta charset="utf-8">
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -20,6 +20,43 @@
     <!-- Compiled and minified JavaScript -->
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>-->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <style>
+      .centro{
+        margin-left: 900px;
+      }
+  
+      .logo1{
+        margin-left: 200px;
+      }
+      .logo2{
+        margin-right: 100px;
+      }
+    
+              .topicos{
+		            font-family: "Roboto", sans-serif;
+		            font-size: 17px;
+		            font-weight: 800;
+                margin-left: 43px;;
+	            }
+              .topico5{
+		            font-family: "Roboto", sans-serif;
+		            font-size: 17px;
+		            font-weight: 800;
+
+	            }
+                .topico7{
+		            font-family: "Roboto", sans-serif;
+		            font-weight: 800;
+	            }
+            .logar{
+              font-weight: bold;
+            }
+            .cont2{
+		font-family: "Lilita One", sans-serif;
+		font-size: 50px;
+	}
+    </style>
 </head>
 
 <body>
@@ -35,14 +72,33 @@
     }
     ?>
 
-    <h2>Bem-vindo à Página Inicial</h2>
-    <a href="logout.php">Logout</a><br>
-    <a href="cadastrarCelular.php">Celulares</a>
+<div class="navbar-fixed">
+      <nav>
+        <div class="nav-wrapper cyan accent-3 font2">
+          <a href="paginaInicial.php" class="brand-logo logo1">DeTudo.com</a>
+          <ul class="right hide-on-med-and-down logo2">
+            <li><a href="home.php">Usuários</a></li>
+            <li><a href="crudMac.php">Itens</a></li>
+            <li><a href="cadastrarCelular.php">Celulares</a></li>
+            <li><a class="black-text" href="logout.php"><i class="material-icons">power_settings_new</i></a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    </div>
+  
+    <br><br>
+    <div class="row">
+    <div class="col s9 offset-s1 titulo">
+       <h3>Listagem de Usuários</h3>
+    </br>
+    </div>
+    </div>
 
-    <h3>Listagem de Usuários</h3>
-
-    <table border="1">
-        <thead>
+    <div class="row">
+    <article class="col s10 offset-s2 ">
+    <table class="responsive-table striped ftabela" border="1">
+        <thead class="titulo">
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
@@ -53,19 +109,34 @@
             <!-- A lista de usuários será carregada aqui usando Ajax -->
         </tbody>
     </table>
+    </article>
+</div>
+<br> <br> <br>
     
     <div id="modalEditar" class="modal">
     <div class="modal-content">
-        <h4>Editar Usuário</h4>
+        <h4 class="titulo">Editar Usuário</h4>
+        <br>
+
         <form id="formEditar">
-            <input type="hidden" id="editId" name="id">
-            <label for="editNome">Nome:</label>
-            <input type="text" id="editNome" name="nome" required><br>
 
-            <label for="editEmail">Email:</label>
-            <input type="email" id="editEmail" name="email" required><br>
+        <div class="input-field prefix.active">
+        <input type="hidden" id="editId" name="id">
+        </div>
 
-            <button type="submit">Salvar</button>
+        <div class="input-field prefix.active">
+        <i class="material-icons prefix">perm_identity</i>
+        <label for="editNome">Nome:</label>
+        <input type="text" id="editNome" name="nome" required><br>
+        </div>
+
+        <div class="input-field prefix.active">
+        <i class="material-icons prefix">mail_outline</i>
+        <label for="editEmail">Email:</label>
+        <input type="email" id="editEmail" name="email" required><br>
+        </div>
+
+        <button class="waves-effect waves-light btn  white-text cyan accent-3 logar" type="submit">Salvar</button>
         </form>
     </div>
     <div class="modal-footer">
@@ -100,8 +171,8 @@
 
                     for (var i = 0; i < data.length; i++) {
                         listaUsuarios.append('<tr><td>' + data[i].id + '</td><td>' + data[i].nome + '</td><td>' + data[i].email + '</td>' +
-                            '<td><a class="waves-effect waves-light btn modal-trigger" href="#modalEditar" onclick="preencherFormEditar(' + data[i].id + ')">Editar</a>' +
-                            '<a class="waves-effect waves-light btn modal-trigger" href="#modalExcluir" onclick="preencherFormExcluir(' + data[i].id + ')">Excluir</a></td></tr>');
+                            '<td><button class="waves-effect waves-light btn white-text cyan accent-3 logar modal-trigger" href="#modalEditar" onclick="preencherFormEditar(' + data[i].id + ')">Editar</button>' + 
+                            '<button class="waves-effect waves-light btn white-text cyan accent-3 logar modal-trigger" href="#modalExcluir" onclick="preencherFormExcluir(' + data[i].id + ')">Excluir</button></td></tr>');
                     }
                 },
                 error: function (xhr, status, error) {
